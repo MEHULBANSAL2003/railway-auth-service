dcda984dba734223f557fdce940ac1746a52e7b9
MEHULBANSAL2003 <mehulbansal83606@gmail.com>
2026-01-24 22:26:33 +0530

    refresh token functionality implemented

--- src/main/java/com/railway/auth_service/controller/AuthController.java
+++ src/main/java/com/railway/auth_service/controller/AuthController.java
@@ -9,7 +9,6 @@ import com.railway.auth_service.service.authService.AuthServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
